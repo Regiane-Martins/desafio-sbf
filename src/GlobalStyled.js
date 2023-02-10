04f145b582ef9dc2e70453ace15fbceada804cd2
Regiane-Martins <regianegmartins@hotmail.com>
2023-02-10 19:26:06 -0300

    mudar forma de importar a font

--- src/GlobalStyled.js
+++ src/GlobalStyled.js
@@ -5,7 +5,6 @@ export const GlobalStyled = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    @import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Poppins:wght@200;400;700&family=Roboto&display=swap");
     font-family: "Roboto", sans-serif;
 
     body {
